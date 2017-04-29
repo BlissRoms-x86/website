4612c5bc0885c216fbd273545d6469fdeb53a065
electrikjesus <electrikjesus@gmail.com>
2017-04-28 20:58:21 -0400

    add mock brunch functions

--- script.js
+++ script.js
@@ -321,6 +321,12 @@ $(document).ready(function(e) {
                log("Client", "Not enough arguments to log in, you need a USERNAME and a PASSWORD.");
             }
             break;
+         case "brunch bliss_kiwi":
+            window.open("https://downloads.blissroms.com/Bliss/Official/kiwi/");
+            break;
+         case "brunch bliss_x86":
+            window.open("https://downloads.blissroms.com/Bliss/Official/x86/");
+            break;
          default:
             output = "Unrecognised command '" + word[0] + "'.";
             log("Client", output);
