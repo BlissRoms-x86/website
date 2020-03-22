5aa3fd82fecc292b8405ecdcf8eb04cfa408e748
Jon West <electrikjesus@gmail.com>
2020-03-22 19:25:47 -0400

    Bliss-Bot: Add a "Karen" clause

--- js/chat.js
+++ js/chat.js
@@ -442,8 +442,8 @@ $(document).ready(function(){
 			var message="For those looking where to begin, I would like to direct your attention to our docs.blissroms.com site. The Bliss OS (x86) section on it has all the possible knowledge we've gathered to help make things easier for you all to find, and better for our sanity from having to repeat ourselves. Start there, and make sure to check out the XDA thread for the build you're using (nougat, oreo, pie, q) for any questions that you couldn't figure out using the docs site.";
 		} else if(string.includes("wheres_the_keymapper")||string.includes("keymapper")){
 			var message="Q: Why is there no keymapper included in Bliss OS? A: We have not found any open source keymappers, so we won't put our users data in jeopardy just for that. We are open to working with any developers interested in creating an on-screen keymapper for keyboard/mouse and gampads though, so please spread the word. For those lost as to why we want to ensure security there, a gaming keymapper will redirect any input from the users device, and can even run when a game is not loaded. So it has the potential to grab passwords, banking info, etc. We don't trust anything we can't sift through the source of enough these days, so this is why we haven't included one as of yet";
-		} else if(string.includes("updates")){
-			var message="Get yourself a Telegram account, then Subscribe to this Channel for All announcements and updates https://t.me/blissos_updates";
+		} else if(string.includes("your manager")||string.includes("real person")||string.includes("speak with a human")){
+			var message="We do have real people available to help in our free Telegram chat for Bliss OS. https://t.me/blissx86 Feel free to sign up for an account and ask more questions in there. ";
 		}
 
 		var typeDelay=300+(message.length*25)+(Math.random()*1000);
